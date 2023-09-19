eec20b4a8fc4e16df3134a9e5359ad517ab47ed7
DarshanaBombuwala <136680033+DarshanaBombuwala@users.noreply.github.com>
2023-09-19 10:32:37 +0530

    Update 3.scala

--- 3.scala
+++ 3.scala
@@ -1,12 +1,19 @@
-object Q3 {
-
-  def filterPrime(li:List[Int]): List[Int] = {
-    val prime = li.map((x:Int) => )
+object PrimeFilter {
+  
+  val isPrime: Int => Boolean = (n: Int) => {
+    if (n <= 1) false
+    else if (n == 2) true
+    else !(2 to Math.sqrt(n).toInt).exists(x => n % x == 0)
   }
 
-  def main(args: Array[String]): Unit ={
-    val num = List(1,2,3,4,5,6,7,8,9,10)
-
+  def filterPrime(numbers: List[Int]): List[Int] = {
+    numbers.filter(isPrime)
   }
 
+  def main(args: Array[String]): Unit = {
+    val inputList = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11)
+    val primeNumbers = filterPrime(inputList)
+    println("Prime numbers: " + primeNumbers)
+  }
 }
+
